1fefb70c1eaaad996c8896eeda7b845eae898b48
Scott Sanbar <scott.sanbar@gmail.com>
2014-10-18 13:56:23 -0500

    Added argument error checking, updated unit tests

--- src/add.js
+++ src/add.js
@@ -4,10 +4,26 @@
 'use strict';
 
 function add(vec1, vec2) {
-    return vec1.reduce(function(acc, elem, index) {
-        acc.push(elem + vec2[index]);
-        return acc;
-    }, []);
+    if (arguments.length !== 2) {
+        throw new TyperError('You must pass add exactly two array arguments (vectors)')
+    }
+    if (!Array.isArray(vec1) || !Array.isArray(vec2)) {
+        throw new TypeError('Both arguments (vectors) must be arrays');
+    }
+    if (vec1.length === 0 || vec2.length === 0) {
+        throw new TypeError('Both arguments (vectors) must contain at least 1 element');
+    }
+    if (vec1.length !== vec2.length) {
+        throw new TypeError('Both arguments (vectors) must have the same array length');
+    }
+    return vec1.map(function(elem1, index) {
+        var elem2 = vec2[index];
+        if (typeof elem1 !== 'number' || typeof elem2 !== 'number' ||
+            isNaN(elem1) || isNaN(elem2)) {
+            throw new TypeError('All vector elements must be valid numbers');
+        }
+        return elem1 + elem2;
+    });
 }
 
 module.exports = add;
--- test/test.js
+++ test/test.js
@@ -14,15 +14,26 @@ var test = require('tape'),
     vec31 = [1.2, -2, 3.5],
     vec32 = [4, 5, -6],
     vec41 = [-1.1, 2, -3.3, 4],
-    vec42 = [5, -6.6, -7, 8];
+    vec42 = [5, -6.6, -7, 8],
+    vecAlpha = ['a'],
+    vecNaN = [NaN];
 
 test('adds two n-dimensional vectors', function(t) {
     t.equal(Array.isArray(add(vec21, vec22)), true, 'returned result is an array');
-    t.deepEqual(add(vec0, vec0), vec0, '0 dimensional vector test');
     t.deepEqual(add(vec11, vec12), [3], '1 dimensional vector test');
     t.deepEqual(add(vec21, vec22), [-2, 1.5], '2 dimensional vector test');
     t.deepEqual(add(vec31, vec32), [5.2, 3, -2.5], '3 dimensional vector test');
     t.deepEqual(add(vec41, vec42), [3.9, -4.6, -10.3, 12], '4 dimensional vector test');
+    t.throws(function() { return add(); }, null, 'You must pass add exactly two array arguments (vectors)');
+    t.throws(function() { return add(vec11); }, null, 'You must pass add exactly two array arguments (vectors)');
+    t.throws(function() { return add(vec11, vec12, vec12); }, null, 'You must pass add exactly two array arguments (vectors)');
+    t.throws(function() { return add(vec0, vec12); }, null, 'Both arguments (vectors) must contain at least 1 element');
+    t.throws(function() { return add(vec11, vec0); }, null, 'Both arguments (vectors) must contain at least 1 element');
+    t.throws(function() { return add(vec11, vecAlpha); }, null, 'All vector elements must be valid numbers');
+    t.throws(function() { return add(vecAlpha, vec11); }, null, 'All vector elements must be valid numbers');
+    t.throws(function() { return add(vecAlpha, vec11); }, null, 'All vector elements must be valid numbers');
+    t.throws(function() { return add(vecNaN, vec12); }, null, 'All vector elements must be valid numbers');
+    t.throws(function() { return add(vec11, vecNaN); }, null, 'All vector elements must be valid numbers');
     t.end();
 });
 test('subtracts two n-dimensional vectors', function(t) {
